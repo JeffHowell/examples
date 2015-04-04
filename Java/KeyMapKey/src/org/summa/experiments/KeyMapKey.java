e13c8d3b79f8a0b4bf08731746cf0f5b158441ff
JeffHowell <jhowell@summa-tech.com>
2015-04-04 07:45:26 -0400

    add test called 'usage' with lots of comments to show how a KeyMapKey might be used.

--- Java/KeyMapKey/src/org/summa/experiments/KeyMapKey.java
+++ Java/KeyMapKey/src/org/summa/experiments/KeyMapKey.java
@@ -37,81 +37,81 @@ public final class KeyMapKey<L, R> {
     public final Map<R, L> right = new MapMap<R, L>(rMap, lMap);
 
     private class MapMap<MY, OT> implements Map<MY, OT> {
-	private final Map<MY, OT> myMap;
-	private final Map<OT, MY> otherMap;
+        private final Map<MY, OT> myMap;
+        private final Map<OT, MY> otherMap;
 
-	private MapMap(Map<MY, OT> myMap, Map<OT, MY> otherMap) {
-	    this.myMap = myMap;
-	    this.otherMap = otherMap;
-	}
+        private MapMap(Map<MY, OT> myMap, Map<OT, MY> otherMap) {
+            this.myMap = myMap;
+            this.otherMap = otherMap;
+        }
 
-	@Override
-	public int size() {
-	    return myMap.size();
-	}
+        @Override
+        public int size() {
+            return myMap.size();
+        }
 
-	@Override
-	public boolean isEmpty() {
-	    return myMap.isEmpty();
-	}
+        @Override
+        public boolean isEmpty() {
+            return myMap.isEmpty();
+        }
 
-	@Override()
-	public boolean containsKey(Object key) {
-	    return myMap.containsKey(key);
-	}
+        @Override()
+        public boolean containsKey(Object key) {
+            return myMap.containsKey(key);
+        }
 
-	@Override
-	public boolean containsValue(Object value) {
-	    return myMap.containsValue(value);
-	}
+        @Override
+        public boolean containsValue(Object value) {
+            return myMap.containsValue(value);
+        }
 
-	@Override
-	public OT get(Object key) {
-	    return myMap.get(key);
-	}
+        @Override
+        public OT get(Object key) {
+            return myMap.get(key);
+        }
 
-	@Override
-	public synchronized OT put(MY key, OT value) {
-	    otherMap.put(value, key);
-	    return myMap.put(key, value);
-	}
+        @Override
+        public synchronized OT put(MY key, OT value) {
+            otherMap.put(value, key);
+            return myMap.put(key, value);
+        }
 
-	@Override
-	synchronized public OT remove(Object key) {
-	    // noinspection SuspiciousMethodCalls
-	    otherMap.remove(myMap.get(key));
-	    return myMap.remove(key);
-	}
+        @Override
+        public synchronized OT remove(Object key) {
+            // noinspection SuspiciousMethodCalls
+            otherMap.remove(myMap.get(key));
+            return myMap.remove(key);
+        }
 
-	@Override
-	public void putAll(Map<? extends MY, ? extends OT> m) {
-	    for (Entry<? extends MY, ? extends OT> entry : m.entrySet()) {
-		MY myKey = entry.getKey();
-		OT otherKey = entry.getValue();
+        @Override
+        public void putAll(Map<? extends MY, ? extends OT> m) {
+            for (Entry<? extends MY, ? extends OT> entry : m.entrySet()) {
+                MY myKey = entry.getKey();
+                OT otherKey = entry.getValue();
 
-		put(myKey, otherKey);
-	    }
-	}
+                put(myKey, otherKey);
+            }
+        }
 
-	@Override
-	synchronized public void clear() {
-	    otherMap.clear();
-	    myMap.clear();
-	}
+        @Override
+        public synchronized void clear() {
+            otherMap.clear();
+            myMap.clear();
+        }
 
-	@Override
-	public Set<MY> keySet() {
-	    return myMap.keySet();
-	}
+        @Override
+        public Set<MY> keySet() {
+            return myMap.keySet();
+        }
 
-	@Override
-	public Collection<OT> values() {
-	    return myMap.values();
-	}
+        @Override
+        public Collection<OT> values() {
+            return myMap.values();
+        }
 
-	@Override
-	public Set<Entry<MY, OT>> entrySet() {
-	    return myMap.entrySet();
-	}
+        @Override
+        public Set<Entry<MY, OT>> entrySet() {
+            return myMap.entrySet();
+        }
     }
 }
